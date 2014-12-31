384c7fa3e3401170c68a011e90748af38e63f594
Robert D Glover Jr <RobertGloverJr@gmail.com>
2014-12-31 06:45:16 -0500

    added link to documentation on js as a comment to program4.js

--- program4.js
+++ program4.js
@@ -5,6 +5,8 @@ fs.readFile(fileToRead,'utf8', function(err, strData) {
   if (err) throw err;
   console.log(strData.split('\n').length-1);
 });
+// file:///usr/local/lib/node_modules/learnyounode/node_apidoc/fs.html
+//
 /*
 Here's the official solution in case you want to compare notes:
 
