a77d7e8db7a6dd7824cf95be2bb8f0974b134492
Robert D Glover Jr <RobertGloverJr@gmail.com>
2015-01-02 08:19:54 -0500

    after commit will change logic of of callback

--- module6.js
+++ module6.js
@@ -1,27 +1,35 @@
 module.exports = function(fullPathToDir, fileExtension, callBackFunc) {
+  console.log('module6:: logic point 1');
   var fs = require('fs')
   var path = require('path')
   var filteredList = [];
   var saveErr;
 
-  console.log('fullPathToDir='+fullPathToDir);
-  console.log('fileExtension='+fileExtension);
+  console.log('module6::fullPathToDir='+fullPathToDir);
+  console.log('module6::fileExtension='+fileExtension);
+  console.log('module6:: logic point 2');
 
-
-  fs.readdir(fullPathToDir, function (err, list) {
+  fs.readdir('/Users/rdg/Documents/rdgdev/NodeJs/NodeSchool/learnyounode', function (err, list) {
+    console.log('module6:: logic point 3');
     if (err) {
-      console.log('rdg: err='+fullPathToDir);
+      console.log('module6:: logic point 4');
+      console.log('module6::: err='+fullPathToDir);
       return callBackFunc(err);
     }
     list.forEach(function (file) {
+      console.log('module6:: logic point 5');
       if (path.extname(file) === fileExtension) {
+        console.log('module6:: logic point 6');
         //filteredList[filteredList.length] = file;
         filteredList.push(file);
-        console.log('rdg: file='+file + ' filteredList.length='+filteredList.length );
+        console.log('module6:: file='+file + ' filteredList.length='+filteredList.length );
       }
     });
   });
+  console.log('module6:: logic point 7');
     console.log('module6:: filteredList='+filteredList);
     console.log('module6::: filteredList.length='+filteredList.length);
+    console.log('module6:: logic point 8');
     return callBackFunc(null, filteredList);
+    console.log('module6:: logic point 9');
 }
--- program6.js
+++ program6.js
@@ -1,17 +1,30 @@
+console.log('program6:: logic point 1');
 var myModule6 = require('./module6.js');
+
+console.log('program6:: logic point 2');
 var fullPathToDir = process.argv[2];
+
+console.log('program6:: logic point 3');
 var fileExtension = "." + process.argv[3];
+
+console.log('program6:: logic point 4');
 var myCallbackFunc = function(err, arrayOfFilenames){
-  console.log("rdg: enterred myCallbackFunc");
+  console.log('program6:: logic point 5');
   if (err) {
-    console.log('rdg: detected err');
+    console.log('program6:: logic point 6');
     console.log(err);
   } else {
-    console.log('rdg: else');
+    console.log('program6:: logic point 7');
     arrayOfFilenames.forEach(function(faVal, faIndex, faArray) {
-      console.log(faVal);
+      console.log('program6:: logic point 8');
+      console.log('rdg(forEach faVal): '+faVal);
+      console.log('rdg(forEach faIndex): '+faIndex);
+      console.log('rdg(forEach faArray): '+faArray);
     });
   }
-  console.log('rdg: exiting myCallbackFunc');
+  console.log('program6:: logic point 9');
 };
+console.log('program6:: logic point 10');
 myModule6(fullPathToDir, fileExtension, myCallbackFunc);
+
+console.log('program6:: logic point 11');
