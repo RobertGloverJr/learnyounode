aa85932f27c791418597ab8fcb4916b0dbc91bc6
Robert D Glover Jr <RobertGloverJr@gmail.com>
2015-01-10 12:27:42 -0500

    program7.js is able to print returned html but not formatted correctly

--- program7.js
+++ program7.js
@@ -6,11 +6,11 @@
 arg_url_to_get = process.argv[2];
 nodejs_http = require('http');
 nodejs_http.get(arg_url_to_get, function(response){
-  console.log("rdg: got response: " + response.statusCode);
-}).on('error', function(err){
-   console.log("rdg: Got error: " + err.message);
-}).on('end', function(x){
-  console.log("rdg: Got end.");
-}).on('data', function(dataStream){
-  console.log("rdg: got data");
+  console.log("rdg: got a response: " + response.statusCode);
+  console.log("rdg: response.httpVersion: " + response.httpVersion);
+  response.setEncoding("utf8");
+  response.on("data", function (theData){
+    console.log("typeof data: " + typeof data);
+    console.log("got data!!!!-->" + theData);
+  });
 });
